8b8656c7f325e650cf78ffff15db4ec6c4da5297
JeongHyunHeo <21700786@handong.edu>
2020-04-24 02:55:12 +0000

    Initial commit

---
+++ base.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "base.h"
+
+// Function: print_all_records()
+// Input: record - array of Records; this may contain empty elements in the middle
+// Output: none
+// - Leave a brief information about the function
+void print_all_records(Record records[]){
+	// TODO: Modify this function as you need
+	printf("print_all_records(): this function needs to be implemented\n\n");
+}
+
+
+// Function: add_a_record()
+// Input: record - array of Records; this may contain empty elements in the middle
+// Output: none
+// - Leave a brief information about the function
+void add_a_record(Record records[]){
+	// TODO: Modify this function as you need
+	printf("add_a_record(): this function needs to be implemented\n\n");
+}
+
+
+// TODO: Add more functions to fulfill the requirements
---
+++ record.h
@@ -0,0 +1,12 @@
+#ifndef _RECORD_H_
+#define _RECORD_H_
+
+// type defition
+typedef struct{
+	int id;
+
+	// TODO: define your own structure type here
+
+} Record;
+
+#endif
