6923d30c278ac030f3d18bb80d767eedd7639493
JeongHyunHeo <63202345+JeongHyunHeo@users.noreply.github.com>
2020-04-28 14:03:12 +0900

    Update base.h

--- base.h
+++ base.h
@@ -3,8 +3,11 @@
 
 #include "record.h"
 
-// function prototypes
-void print_all_records(Record[]);
-void add_a_record(Record[]);
-
+void print_book(Record records[], int* count);
+void add_book(Record records[], int* count);
+void search_book(Record records[], int* count);
+int delete_book(Record records[], int* count);
+int update_book(Record records[], int* count);
+int saveFile(Record records[], int* count);
+int openfile(Record records[], int* count);
 #endif
