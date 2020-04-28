684e26a0699406a656d0ea9e0d563aed26756d67
JeongHyunHeo <63202345+JeongHyunHeo@users.noreply.github.com>
2020-04-28 14:05:06 +0900

    Update record.h

--- record.h
+++ record.h
@@ -1,12 +1,18 @@
 #ifndef _RECORD_H_
 #define _RECORD_H_
 
-// type defition
+#define MAX_BOOKNAME 50
+#define MAX_AUTHOR 50
+#define MAX_GENRE 20
+#define MAX_QUANTITY 10
+#define MAX_ISBN 20
+
 typedef struct{
-	int id;
-
-	// TODO: define your own structure type here
-
+  char bookname[MAX_BOOKNAME];
+  char author[MAX_AUTHOR];
+  char genre[MAX_GENRE];
+  int quantity;
+  char isbn[MAX_ISBN];
 } Record;
 
 #endif
