a1c733f03420bae8c6cce4adecb63ed0d8ae4b9e
JeongHyunHeo <63202345+JeongHyunHeo@users.noreply.github.com>
2020-05-12 11:02:14 +0900

    Update record.h

--- record.h
+++ record.h
@@ -1,18 +1,14 @@
 #ifndef _RECORD_H_
 #define _RECORD_H_
 
-#define MAX_BOOKNAME 50
-#define MAX_AUTHOR 50
-#define MAX_GENRE 20
-#define MAX_QUANTITY 10
-#define MAX_ISBN 20
+#define MAX 64
 
 typedef struct{
-  char bookname[MAX_BOOKNAME];
-  char author[MAX_AUTHOR];
-  char genre[MAX_GENRE];
+  char bookname[MAX];
+  char author[MAX];
+  char genre[MAX];
   int quantity;
-  char isbn[MAX_ISBN];
+  char isbn[MAX];
 } Record;
 
 #endif
